94dfc78210a93abca033a088153a4ead94b26e49
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-07-09 16:02:10 -0700

    pkg/testutils: use testsql and setup proper localstack creds

--- pkg/testutils/s3.go
+++ pkg/testutils/s3.go
@@ -2,8 +2,20 @@ package testutils
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/mitchellh/go-testing-interface"
 )
+
+func AWSSession(t testing.T) *session.Session {
+	return session.New(aws.NewConfig().
+		WithEndpoint("http://localhost:4566").
+		WithRegion("us-east-1").
+		WithCredentials(credentials.NewStaticCredentials("hzn", "hzn", "hzn")).
+		WithS3ForcePathStyle(true),
+	)
+}
 
 func DeleteBucket(api *s3.S3, bucket string) {
 	var marker *string
