3dba5d3c8f6e7b0f36bc096deeb5f5cd417f57c7
Evan Phoenix <evan@phx.io>
2020-07-29 12:12:46 -0700

    Make labels case insensitive

--- pkg/pb/label_test.go
+++ pkg/pb/label_test.go
@@ -17,6 +17,18 @@ func TestLabelSet(t *testing.T) {
 		require.False(t, ls.Matches(target))
 	})
 
+	t.Run("uses case folding on match", func(t *testing.T) {
+		ls := ParseLabelSet("service=www,env=prod,instance=aabbcc")
+
+		target := ParseLabelSet("service=www,env=prod")
+
+		target.Labels[1].Name = "ServICE"
+		target.Labels[1].Value = "WWW"
+
+		require.True(t, target.Matches(ls))
+		require.False(t, ls.Matches(target))
+	})
+
 	t.Run("is stabley sorted", func(t *testing.T) {
 		ls := ParseLabelSet("service=emp,env=test")
 		assert.Equal(t, "env", ls.Labels[0].Name)
@@ -40,4 +52,18 @@ func TestLabelSet(t *testing.T) {
 		ls = ParseLabelSet("service=foo,service=emp")
 		assert.Equal(t, "foo", ls.Labels[1].Value)
 	})
+
+	t.Run("lowercases on parsing", func(t *testing.T) {
+		ls := ParseLabelSet("service=emp,ENV=test")
+		assert.Equal(t, "env", ls.Labels[0].Name)
+
+		ls.Finalize()
+
+		assert.Equal(t, "env", ls.Labels[0].Name)
+
+		ls.Finalize()
+
+		assert.Equal(t, "env", ls.Labels[0].Name)
+	})
+
 }
