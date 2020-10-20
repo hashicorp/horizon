8d5f4dbd5b97eb6897768d857f23f1dfae1d4e1c
Evan Phoenix <evan@phx.io>
2020-10-20 13:46:13 -0700

    cmd/ulidgen: quick tool to help decode ulid's in postgres

--- cmd/ulidgen/main.go
+++ cmd/ulidgen/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
+	"os"
 
 	"github.com/hashicorp/horizon/pkg/pb"
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		rep := os.Args[1]
+
+		data, err := hex.DecodeString(rep)
+		if err != nil {
+			panic(err)
+		}
+
+		idx := bytes.IndexByte(data, '!')
+		if idx != -1 {
+			data = data[idx+1:]
+		}
+
+		fmt.Println(pb.ULIDFromBytes(data).String())
+		return
+	}
+
 	fmt.Println(pb.NewULID().String())
 }
